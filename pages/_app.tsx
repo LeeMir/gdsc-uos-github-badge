c6c13c0a96606b87efa6330f48edc0606e0b2b8f
LeeMir <manofgodmj@gmail.com>
2021-12-19 21:26:20 +0900

    [Chore] favicon 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import { Global } from '@emotion/react';
 import { reset } from '../styles/global';
 
 const App = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <link rel='shortcut icon' href='/favicon.ico' />
+    </Head>
     <Global styles={reset} />
     <Component {...pageProps} />
   </>
