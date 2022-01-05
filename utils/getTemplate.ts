16a547211ca9cc55ece61a11fdb06a471314d18b
LeeMir <manofgodmj@naver.com>
2022-01-05 18:03:20 +0900

    [Style] if - if => switch로 변경

--- utils/getTemplate.ts
+++ utils/getTemplate.ts
@@ -2,11 +2,18 @@ import { ROLE, VERSION } from './constants';
 import * as svgTemplate from './svgTemplate';
 
 export const role2Str = (role): string => {
-  if (role === ROLE.MEMBER) return 'Member';
-  if (role === ROLE.CORE) return 'Core Member';
-  if (role === ROLE.LEAD) return 'Lead';
-  if (role === ROLE.ALUMNI) return 'Alumni';
-  return '';
+  switch (role) {
+    case ROLE.MEMBER:
+      return 'Member';
+    case ROLE.CORE:
+      return 'Core Member';
+    case ROLE.LEAD:
+      return 'Lead';
+    case ROLE.ALUMNI:
+      return 'Alumni';
+    default:
+      return '';
+  }
 };
 
 const getTemplate = (
@@ -15,19 +22,22 @@ const getTemplate = (
   posts: number,
   version: string
 ) => {
-  if (version === VERSION.BASIC)
-    return svgTemplate.basicTemplate(name, role, posts);
-  if (version === VERSION.YELLOW)
-    return svgTemplate.yellowTemplate(name, role, posts);
-  if (version === VERSION.GREEN)
-    return svgTemplate.greenTemplate(name, role, posts);
-  if (version === VERSION.RED)
-    return svgTemplate.redTemplate(name, role, posts);
-  if (version === VERSION.BLUE)
-    return svgTemplate.blueTemplate(name, role, posts);
-  if (version === VERSION.ALUMNI)
-    return svgTemplate.alumniTemplate(name, role, posts);
-  return '';
+  switch (version) {
+    case VERSION.BASIC:
+      return svgTemplate.basicTemplate(name, role, posts);
+    case VERSION.YELLOW:
+      return svgTemplate.yellowTemplate(name, role, posts);
+    case VERSION.GREEN:
+      return svgTemplate.greenTemplate(name, role, posts);
+    case VERSION.RED:
+      return svgTemplate.redTemplate(name, role, posts);
+    case VERSION.BLUE:
+      return svgTemplate.blueTemplate(name, role, posts);
+    case VERSION.ALUMNI:
+      return svgTemplate.alumniTemplate(name, role, posts);
+    default:
+      return '';
+  }
 };
 
 export default getTemplate;
